3ff55f60ae3c93e5f19858e0dc46464f2434656e
Rubika-hacker <127617245+Rubika-hacker@users.noreply.github.com>
2023-05-22 01:26:08 +0330

    Create filter.rubika.py

---
+++ filter.rubika.py
@@ -0,0 +1,15 @@
+print('https://github.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika/blob/main/xvideos.com_e54a99aa3c763741e76b40f5be859df8.mp4?raw=true') 
+
+ print('https://github.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika/blob/main/xvideos.com_b4e4537740b672253e9c7b012e80a182.mp4?raw=true') 
+
+ print('https://github.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika/raw/main/kali-roses-banged-by-a-group-of-black-guys_l13x.gif') 
+
+ print('https://s6.uupload.ir/files/pjr.jpeg') 
+
+ print('FILTER') 
+
+ print('/yft15k') 
+
+ print('http://yttk16.22.22.Insulting.the.leader.and.obscenity.rest') 
+
+ end()را
